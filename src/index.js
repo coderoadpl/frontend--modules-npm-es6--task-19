4aae97801caee96a17946d94d6de1362f6fe71c1
Mateusz Choma <kontakt@coderoad.pl>
2020-09-07 08:03:26 +0200

    Step 1 - destructuring by defining normal variables

--- src/index.js
+++ src/index.js
@@ -1,31 +1,10 @@
-// const name1 = 'Ola'
-// const name2 = 'Ala'
-// const name3 = 'Ela'
+const add = (numbers) => {
+    const a = numbers[0]
+    const b = numbers[1]
+    
+    return  a + b
+}
 
-// const names = [name1, name2, name3]
+const numbers = [1, 3]
 
-// console.log(names)
-// console.log(name1)
-// console.log(name2)
-// console.log(name3)
-
-const names = ['Ala', 'Ola', 'Ela']
-
-// const name1 = names[0]
-// const name2 = names[1]
-// const name3 = names[2]
-
-const [name1, name2, name3] = names
-
-console.log(names)
-console.log(name1)
-console.log(name2)
-console.log(name3)
-
-const numbers = [1, 2, 3]
-
-// const three = numbers[2]
-
-const [, , three] = numbers
-
-console.log(three)
+console.log(add(numbers))
